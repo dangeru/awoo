f9f60390679681742bcbf9e6ca2f58029497ce2c
Niles Rogoff <me@niles.xyz>
2017-08-16 15:58:37 -0700

    Legacy api

--- create.sql
+++ create.sql
@@ -12,16 +12,3 @@ CREATE TABLE posts (
 	date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	title TEXT NOT NULL DEFAULT ''
 );
-
-INSERT INTO posts (board, post_id, parent, content) VALUES
-	('test', 0, 0, 'This is an op'),
-	('test', 1, 0, 'This is a reply to post number zero'),
-	('test', 2, 2, 'This is another op'),
-	('test', 3, 3, 'This is a third OP with four replies'),
-	('test', 4, 3, 'This is reply number 1 to the third OP'),
-	('test', 5, 3, 'This is reply number 2 to the third OP'),
-	('test', 6, 3, 'This is reply number 3 to the third OP'),
-	('test', 7, 3, 'This is reply number 4 to the third OP');
-
-UPDATE posts SET title = 'Title for ' || content WHERE post_id = parent;
-
--- src/routes/boards.rb
+++ src/routes/boards.rb
@@ -15,6 +15,7 @@ module Sinatra
         def self.registered(app)
           config_raw = File.read('config.json')
           config = JSON.parse(config_raw)
+          hostname = config["hostname"]
           app.set :config, config
           boards = []
           config['boards'].each do |key, array|
@@ -56,6 +57,47 @@ module Sinatra
               erb :thread, :locals => {:path => path, :id => id, :con => con}
             end
           end
+
+          # Legacy api, see https://github.com/naomiEve/dangeruAPI
+          app.get "/api.php" do
+            limit = params[:ln]
+            if not limit
+              limit = "10000"
+            end
+            if params[:type] == "thread"
+              id = con.escape(params[:thread].to_i.to_s)
+              result = {:meta => [], :replies => []}
+              limit = con.escape((limit.to_i + 1).to_s)
+              con.query("SELECT * FROM posts WHERE parent = #{id} OR post_id = #{id} LIMIT #{limit}").each do |res|
+                if not res["parent"]
+                  result[:meta] = [{
+                    "title": res["title"],
+                    "id": res["post_id"].to_s,
+                    "url": "https://" + hostname + "/" + params[:board] + "/thread/" + params[:thread]
+                  }]
+                else
+                  result[:replies].push({"post": res["content"]})
+                end
+              end
+            else
+              # type must be index
+              result = {:board => [{
+                :name => config["boards"][params[:board]]["name"],
+                :url => "https://" + hostname + "/" + params[:board]
+              }], :threads => []}
+              limit = con.escape(limit.to_i.to_s)
+              board = con.escape(params[:board]);
+              con.query("SELECT post_id, title, board, COALESCE(parent, post_id) AS effective_parent, COUNT(*)-1 AS number_of_replies FROM posts WHERE board = '#{board}' GROUP BY effective_parent ORDER BY post_id LIMIT #{limit};").each do |res|
+                result[:threads].push({
+                  :id => res["post_id"],
+                  :title => res["title"],
+                  :url => "https://" + hostname + "/" + params["board"] + "/thread/" + res["post_id"].to_s
+                })
+              end
+            end
+            JSON.dump(result)
+          end
+
         end
       end
     end
