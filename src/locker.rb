f97d7c175ad9d769862a3652bf2ae3335a1b75c3
Niles Rogoff <me@niles.xyz>
2017-08-17 11:27:42 -0700

    Fix locking

--- src/locker.rb
+++ src/locker.rb
@@ -1,4 +1,7 @@
+require 'rubygems'
+require 'mysql2'
+require 'json'
 con = Mysql2::Client.new(:host => "localhost", :username => "awoo", :password => "awoo", :database => "test")
 config_raw = File.read(File.dirname(__FILE__) + '/config.json')
 config = JSON.parse(config_raw)
-con.query("UPDATE posts SET is_locked = TRUE WHERE UNIX_TIMESTAMP(CURRENT_TIMESTAMP()) - UNIX_TIMESTAMP(date_posted) > #{config[:seconds_until_archival]} AND parent IS NULL")
+con.query("UPDATE posts SET is_locked = TRUE WHERE UNIX_TIMESTAMP(CURRENT_TIMESTAMP()) - UNIX_TIMESTAMP(date_posted) > #{config["seconds_until_archival"]} AND parent IS NULL")
