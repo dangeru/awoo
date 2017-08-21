980f9d773adcdfe54ef43b29d3f50bccd766c4df
Niles Rogoff <me@niles.xyz>
2017-08-21 08:18:27 -0700

    Optional capcodes, images on burg

--- src/routes/boards.rb
+++ src/routes/boards.rb
@@ -133,15 +133,16 @@ module Sinatra
             ip = get_ip(con, request, env);
             if looks_like_spam(con, ip, env, config) then
               return [403, "Flood detected, post discarded"]
-            end
-            if title.length > 500 or content.length > 500 then
+            elsif title.length > 500 or content.length > 500 then
               return [400, "Post too long (over 500 characters)"]
+            elsif config["boards"][board]["hidden"] and not session[:username]
+              return [403, "You have no janitor permissions"]
             end
             # Check if the IP is banned
             banned = get_ban_info(ip, board, con)
             if banned then return banned end
             # Insert the new post into the database
-            unless session[:username] != nil
+            unless params[:capcode] and session[:username]
               con.query("INSERT INTO posts (board, title, content, ip) VALUES ('#{board}', '#{title}', '#{content}', '#{ip}')");
             else
               con.query("INSERT INTO posts (board, title, content, ip, janitor) VALUES ('#{board}', '#{title}', '#{content}', '#{ip}', '#{session[:username]}')");
@@ -179,9 +180,11 @@ module Sinatra
               return [400, "That thread doesn't exist"]
             elsif closed != 0 then
               return [400, "That thread has been closed"]
+            elsif config["boards"][board]["hidden"] and not session[:username]
+              return [403, "You have no janitor permissions"]
             end
             # Insert the new reply
-            unless session[:username] != nil
+            unless params[:capcode] and session[:username]
               con.query("INSERT INTO posts (board, parent, content, ip, title) VALUES ('#{board}', '#{parent}', '#{content}', '#{ip}', NULL)")
             else
               con.query("INSERT INTO posts (board, parent, content, ip, title, janitor) VALUES ('#{board}', '#{parent}', '#{content}', '#{ip}', NULL, '#{session[:username]}')")
