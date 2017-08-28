a03e40470e7880c36008de61ed3160dd4d771b1d
Niles Rogoff <me@niles.xyz>
2017-08-28 17:59:59 -0400

    Tests finally work

--- src/test/generic_test.rb
+++ src/test/generic_test.rb
@@ -2,8 +2,10 @@ require('minitest/autorun')
 require('net/http')
 require('json')
 require('mysql2')
+require('http-cookie')
 
 API = "/api/v2".freeze
+Dummy_uri = "http://dummy/".freeze
 
 class AwooTest < MiniTest::Test
   def initialize(x)
@@ -13,45 +15,53 @@ class AwooTest < MiniTest::Test
     @time = Time.new.utc - 1
   end
   def test_post
-    assert(not(post_exists("test", "a")))
-    assert_success(post("/post", nil, {"board" => "test", "title" => "a", "comment" => ""}))
-    assert(post_exists("test", "a"))
+    assert(not(find_post("test", "a")))
+    assert(is_success(post("/post", nil, {"board" => "test", "title" => "a", "comment" => ""})))
+    post = find_post("test", "a")
+    assert(post)
+    assert(post["capcode"].nil?)
     cookie = login("test")
-    # TODO test capcode
+    assert(is_success(post("/post", cookie, {"board" => "test", "title" => "b", "comment" => "", "capcode" => "true"})))
+    assert(find_post("test", "b")["capcode"] == "test")
   end
   def test_final_teardown
     Mysql2::Client.new(:host => "localhost", :username => "awoo", :password => "awoo", :database => "awoo").query("DELETE FROM posts WHERE date_posted > TIMESTAMP('#{@time.strftime '%m-%d-%YT%H:%M:%S.0000000'}')")
   end
   private
-  def assert_success(res)
+  def is_success(res)
     res.is_a? Net::HTTPSuccess or res.is_a? Net::HTTPRedirection
   end
-  def post_exists(board, title)
-    JSON.parse(get(API + "/board/" + board).body).each do |post|
+  def find_post(board, title, cookie = nil)
+    JSON.parse(get(API + "/board/" + board, cookie).body).each do |post|
       if post["title"] == title
-        return true
+        return post
       end
     end
     return false
   end
   def login(user)
-    pass = File.open(File.dirname(__FILE__) + "/../config.json") do |f|
+    pass = nil
+    File.open(File.dirname(__FILE__) + "/../config.json") do |f|
       JSON.parse(f.read)["janitors"].each do |mod|
         if mod["username"] == user then
-          mod["password"]
+          pass = mod["password"]
           break
         end
       end
     end
     res = post("/mod", nil, {"username": user, "password": pass})
-    res["Set-Cookie"]
+    jar = HTTP::CookieJar.new
+    res.get_fields("Set-Cookie").each do |value|
+      jar.parse(value, Dummy_uri)
+    end
+    jar
   end
   def get(route, cookie = nil, params = nil)
     uri = URI("http://#{@host}:#{@port}/#{route}")
     uri.query = URI.www_encode_form(params) if params
     Net::HTTP.start(@host, @port) do |http|
       request = Net::HTTP::Get.new uri
-      request.add_field("Set-Cookie", cookie) if cookie
+      request["Cookie"] = HTTP::Cookie.cookie_value(cookie.cookies) if cookie
       http.request request
     end
   end
@@ -59,7 +69,7 @@ class AwooTest < MiniTest::Test
     Net::HTTP.start(@host, @port) do |http|
       request = Net::HTTP::Post.new route
       request.set_form_data(params) if params
-      request.add_field("Set-Cookie", cookie) if cookie
+      request["Cookie"] = HTTP::Cookie.cookie_value(cookie.cookies) if cookie
       http.request request
     end
   end
