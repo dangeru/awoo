e97c2f6234a93446d1958c94776c811706852ecc
Niles Rogoff <me@niles.xyz>
2017-09-06 20:21:39 -0400

    they call me the automatic

--- src/static/static/mobile.js
+++ src/static/static/mobile.js
@@ -23,5 +23,38 @@ if (typeof(unitedPropertiesIf) != "undefined") {
 	mc.style.borderRadius = "0";
 	mc.style.width = "100%";
   }
+} else {
+  var userscript = localStorage.getItem("userscript") == "true";
+  var toggle = function toggle() {
+    localStorage.setItem("userscript", !userscript);
+    window.location.reload();
+  }
+  var options = document.createElement("span");
+  options.style.opacity = 0;
+  options.innerText = "Toggle Userscript (currently "+(userscript ? "on" : "off")+")";
+  options.style.backgroundColor = "white";
+  options.style.color = "black";
+  options.addEventListener("mouseover", function() {
+    options.style.opacity = 1;
+  });
+  options.addEventListener("mouseout", function() {
+    options.style.opacity = 0;
+  });
+  options.addEventListener("click", toggle);
+  options.style.position = "fixed";
+  options.style.top = "10px";
+  options.style.right = "10px";
+  options.style.padding = "3px";
+  options.style.borderRadius = "3px";
+  document.getElementsByTagName("body")[0].appendChild(options);
+  if (userscript) {
+    var s = document.createElement("script");
+    s.src = "/static/awoo-catalog/awoo-catalog.user.js";
+    window.GM_setValue = function GM_setValue(k, v) { localStorage.setItem(k, v); };
+    window.GM_getValue = function GM_getValue(k, d) { 
+      var value = localStorage.getItem(k);
+      return value == null ? d : value;
+    };
+    document.getElementsByTagName("head")[0].appendChild(s);
+  }
 }
-
