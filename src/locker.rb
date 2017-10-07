ce598822913f74d220a520aaee0a50979fa10382
Niles Rogoff <me@niles.xyz>
2017-10-07 17:00:13 -0400

    Update locker and colors

--- src/locker.rb
+++ src/locker.rb
@@ -4,4 +4,4 @@ require 'json'
 con = Mysql2::Client.new(:host => "localhost", :username => "awoo", :password => "awoo", :database => "awoo")
 config_raw = File.read(File.dirname(__FILE__) + '/config.json')
 config = JSON.parse(config_raw)
-con.query("UPDATE posts SET is_locked = TRUE WHERE UNIX_TIMESTAMP(CURRENT_TIMESTAMP()) - UNIX_TIMESTAMP(last_bumped) > #{config["seconds_until_archival"]} AND parent IS NULL")
+con.query("UPDATE posts SET is_locked = TRUE WHERE UNIX_TIMESTAMP(CURRENT_TIMESTAMP()) - UNIX_TIMESTAMP(last_bumped) > #{config["seconds_until_archival"]} AND parent IS NULL AND NOT sticky")
