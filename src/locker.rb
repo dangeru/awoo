311fd1e1c9d7517ebd21e13418a3c9b1e813abca
naomiEve <stapa2000@gmail.com>
2017-10-07 22:47:25 +0200

    why is it referencing a db called test?

--- src/locker.rb
+++ src/locker.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'mysql2'
 require 'json'
-con = Mysql2::Client.new(:host => "localhost", :username => "awoo", :password => "awoo", :database => "test")
+con = Mysql2::Client.new(:host => "localhost", :username => "awoo", :password => "awoo", :database => "awoo")
 config_raw = File.read(File.dirname(__FILE__) + '/config.json')
 config = JSON.parse(config_raw)
 con.query("UPDATE posts SET is_locked = TRUE WHERE UNIX_TIMESTAMP(CURRENT_TIMESTAMP()) - UNIX_TIMESTAMP(last_bumped) > #{config["seconds_until_archival"]} AND parent IS NULL")
