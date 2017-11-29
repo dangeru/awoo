1039e0158f51c65bf965b6ff5a5a3a23f05666d7
Niles Rogoff <me@niles.xyz>
2017-11-29 09:41:52 -0800

    Add character count to replies, fix bug where clicking multiple post numbers would only add it to the reply box once

--- src/static/static/common.js
+++ src/static/static/common.js
@@ -16,3 +16,18 @@ var unstickyPost = function unlockPost(id) {
 var unban = function unban(addr, board) {
 	alert("You tried to unban " + addr + " from the board " + board + ", but it's not implemented yet");
 }
+var doWordcount = function doWordcount(comment_id) {
+	document.getElementById(comment_id).addEventListener("keyup", function() {
+		var comment = document.getElementById(comment_id);
+		console.log(comment.value.length);
+		var len = (comment.value.length * 1.03) | 0;
+		var wc = document.getElementById("wordcount");
+		var submit = document.getElementById("submit");
+		wc.style.color = len > 500 ? "red" : "inherit";
+		if (submit != null) {
+			submit.disabled = len > 500 ? true : false;
+			submit.className = len > 500 ? "special_styled" : "button_styled";
+		}
+		wc.innerText = len + "/500";
+	});
+}
