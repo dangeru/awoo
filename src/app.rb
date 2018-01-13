964866726cf1b5fb140fb534073eaaec94503366
naomiEve <0xlunaric@gmail.com>
2018-01-13 01:59:02 +0100

    make one header even more acoustic

--- src/app.rb
+++ src/app.rb
@@ -37,7 +37,7 @@ class Awoo < Sinatra::Base
   enable :sessions
   before do
     headers "X-Powered-By" => "Awoo (https://github.com/dangeru/awoo)"
-    headers "X-Lolis" => "fbi.gov"
+    headers "X-Loli-Provider" => "fbi.gov"
   end
   get '/' do
     erb :index
