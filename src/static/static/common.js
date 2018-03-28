e166c853cde5539ef26c3d7f1c02274d9eefca89
Niles Rogoff <me@niles.xyz>
2018-03-28 15:40:23 -0400

    Autosize reply box

--- src/static/static/common.js
+++ src/static/static/common.js
@@ -20,7 +20,7 @@ var doWordcount = function doWordcount(comment_id, mod) {
 	var comment = document.getElementById(comment_id);
 	if (comment == null) return;
 	comment.addEventListener("keyup", function() {
-		console.log(comment.value.length);
+		//console.log(comment.value.length);
 		var len = (comment.value.length * 1.03) | 0;
 		var wc = document.getElementById("wordcount");
 		var submit = document.getElementById("submit");
@@ -36,5 +36,22 @@ var doWordcount = function doWordcount(comment_id, mod) {
 			submit.className = (len > 500 && !mod) ? "special_styled" : "button_styled";
 		}
 		wc.innerText = len + "/" + (mod ? "Unlimited" : "500");
+		var lines = comment.value.split("\n");
+		var total_count = lines.length;
+		if (comment.value[comment.value.length - 1] == "\n")
+			total_count++;
+		for (var i = 0; i < lines.length; i++) {
+			var num_chars = lines[i].length;
+			var effective_width = num_chars * 10.3; // approx 10.1 but better be safe
+			var effective_lines = Math.ceil(effective_width / comment.clientWidth);
+			var extra_lines = effective_lines - 1;
+			total_count += extra_lines;
+		}
+		var minimum_height = total_count * 21.5; // approx 21 but better be safe
+		console.log("min: " + minimum_height + " actual: " + comment.style.height)
+		if (comment.style.height == null || comment.style.height == "")
+			comment.style.height = comment.clientHeight + "px"
+		if (Number(comment.style.height.replace("px", "")) < minimum_height)
+			comment.style.height = minimum_height + "px";
 	});
 }
