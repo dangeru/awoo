f124f9bfe180edb4e60797a58c0d63384be428c5
Niles Rogoff <me@niles.xyz>
2018-12-06 19:09:26 -0500

    captcha

--- src/routes/utils/captcha.rb
+++ src/routes/utils/captcha.rb
@@ -2,18 +2,17 @@ require 'mini_magick'
 # this is not recommended
 Encoding.default_internal = Encoding::UTF_8
 Encoding.default_external = Encoding::UTF_8
-#Fonts = %x[convert -list font].split("\n")
-#Fonts = %w(ZXX-Camo ZXX-Noise ZXX-Xed)
-Fonts = %w(ZXX-Noise ZXX-Xed)
-#Fonts.select! do |f| f.strip.split[0] == "Font:" end.map! do |f| f.strip.split[1] end
 Wordlist = "/usr/share/dict/words"
 Words = File.read(Wordlist).split("\n").select! do |w| !w.include?("'") && w.length <= 5 && w.length >= 3 end.map! do |w| w.downcase end
+Memes = ["REOL", "danger/u/", "poi", "burg", "nano", "dorothy", "awoo", "bad touch", "brandtini", "bronson extract", "karmotrine"]
 def make_text
 	rr = ->() do
 		127 + Random.rand(127);
 	end
-	text = "#{Words.sample} #{Words.sample} #{Words.sample}"
-	font = Fonts.sample
+	word = -> () do
+		Random.rand(4) ? Words.sample : Memes.sample
+	end
+	text = "#{word.call} #{word.call} #{word.call}"
 	realtext = ""
 	text.each_codepoint do |x|
 		# keep only ascii lowercase letters and spaces
@@ -25,14 +24,30 @@ def make_text
 	end
 	filename = "/dev/shm/#{realtext}.png"
 	MiniMagick::Tool::Convert.new do |img|
-		img.size "450x60"
+		img.size "450x90"
 		img << "xc:black"
-		img.fill "rgb(#{rr.call}, #{rr.call}, #{rr.call})"
-		#img.stroke "rgb(#{rr.call}, #{rr.call}, #{rr.call})"
+		img.bordercolor "black" # ??
+		img.border "5" # ??
+		color = "rgb(#{rr.call}, #{rr.call}, #{rr.call})"
+		img.fill color
+		img.stroke color
+		img.strokewidth "1"
+		img.font "TimesNewRoman"
 		img.pointsize "40"
-		img.gravity "center"
-		img.font font
-		img.draw "text 0,0 '#{realtext}'"
+		xs = []
+		ys = []
+		x = -450/2
+		realtext.each_codepoint do |c|
+			x = x + (450 / (realtext.length + 1)) + Random.rand(20) - 10
+			y = Random.rand(20) - 10
+			xs << x * 1.1 + (450 / 2)
+			ys << y * 2 + (90 / 2)
+			img.draw "translate #{x},#{y} rotate #{Random.rand(20) - 10} skewX #{Random.rand(35) - (35/2)} gravity center text 0,0 '#{c.chr}'"
+		end
+		img.fill "none"
+		img.strokewidth "4"
+		img.draw "bezier #{xs.zip(ys).flatten[0..15].join(",")}"
+		img.draw "polyline #{xs.zip(ys).flatten[14..-1].join(",")}"
 		img << filename
 	end
 	bytes = File.read filename
